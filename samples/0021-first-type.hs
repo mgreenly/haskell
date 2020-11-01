86f5bd852caabde013367a5f013bd86190db8874
Michael Greenly <mgreenly@gmail.com>
2020-11-01 14:59:18 -0600

    update

--- samples/0021-first-type.hs
+++ samples/0021-first-type.hs
@@ -1,12 +1,23 @@
 --
--- we don't actually have to write the `show` instance for every type, Hasklel can figure out how to do that for
--- us if we ask it with `deriving Show` in the type definition.
+-- 0020-first-type.sh is not idiomatic haskell.
+--
+-- It took some liberties to ensure the concepts werer easily understood.
+--
+-- Typically in haskell we'd always use the same value for the type and the value-constructor.
+--
+-- We'd also allow the compiler to automatically derive the show function for us.
+--
+-- We'd may define the type like this
 --
 
 data Point =
     Point Integer Integer
     deriving Show
 
+--
+-- The function print calls show on it's argument for us, instead of us having to do it if we use putStrLn
+-- so I'd probobally write this
+--
 
 main = do
     let point = Point 5 7
